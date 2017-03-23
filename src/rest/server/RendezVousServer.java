1decef78b7cf8f33161f40267449599f71b4f615
miguel <miguel@miguel-mint>
2017-03-23 12:53:09 +0000

    Add Comments to RendezVousServer Deleted some unnecessary variables

--- src/rest/server/RendezVousServer.java
+++ src/rest/server/RendezVousServer.java
@@ -13,56 +13,69 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class RendezVousServer {
 
-    private static String hostname;
-    private static int port;
-	public static void main(String[] args) throws Exception {
-		port = 8080;
-		if( args.length > 0)
-			port = Integer.parseInt(args[0]);
-                
-		hostname = "http://" + InetAddress.getLocalHost().getHostAddress();
-                
-		URI baseUri = UriBuilder.fromUri(hostname + "/").port(port).build();
+    //base url of this server - contains "http", ip address and port
+    private static URI baseUri;
 
-		ResourceConfig config = new ResourceConfig();
-		config.register( new RendezVousResources() );
-		
-		JdkHttpServerFactory.createHttpServer(baseUri, config);
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
 
-		System.err.println("REST RendezVous Server ready @ " + baseUri + " : local IP = " + InetAddress.getLocalHost().getHostAddress());
-                
-                //Creating Multicast Socket
-                
-		final InetAddress address_multi = InetAddress.getByName("238.69.69.69");
-                
-		if(!address_multi.isMulticastAddress()) {
-			System.out.println( "Use range : 224.0.0.0 -- 239.255.255.255");
-			System.exit(1);
-		}
+        //Setting server
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+        baseUri = UriBuilder.fromUri("http://" + hostAddress + "/").port(port).build();
 
-		MulticastSocket socket = new MulticastSocket(6969);
-		socket.joinGroup(address_multi);
+        ResourceConfig config = new ResourceConfig();
+        config.register(new RendezVousResources());
+        JdkHttpServerFactory.createHttpServer(baseUri, config);
 
-		//Waiting for a client request
-		while(true) {
-			byte[] buffer = new byte[65536];
-			DatagramPacket packet = new DatagramPacket( buffer, buffer.length );
-			socket.receive(packet);
-			processMessage (packet, socket);
-		}
-	}
+        System.err.println("REST RendezVous Server ready @ " + baseUri);
+        //
 
-    private static void processMessage(DatagramPacket packet, MulticastSocket socket) throws IOException {
-      
-        if(new String (packet.getData(), 0, packet.getLength()).equals("RendezVousServer")){
-              
-			String address = String.format("%s:%d/contacts",hostname,port);
+        //Creating Multicast Socket
+        final InetAddress address_multi = InetAddress.getByName("238.69.69.69");
 
-			byte[] input = new String (address).getBytes();
-			DatagramPacket reply = new DatagramPacket( input, input.length );
-			reply.setAddress(packet.getAddress());
-			reply.setPort(packet.getPort());
-			socket.send(reply);
-		}
-	}
+        if (!address_multi.isMulticastAddress()) {
+            System.out.println("Use range : 224.0.0.0 -- 239.255.255.255");
+            System.exit(1);
+        }
+
+        MulticastSocket socket = new MulticastSocket(6969);
+        socket.joinGroup(address_multi);
+
+        //Waiting for a client request
+        while (true) {
+            byte[] buffer = new byte[65536];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+            processMessage(packet, socket);
+        }
+    }
+
+    /**
+     * Processes the messages received in the multicast socket
+     * Checks whether or not the message is meant to this server and if so, it replies with this server location - ip address and port
+     *
+     * @param packet Datagram Packet send by someone and received in the multicast socket
+     * @param socket Multicast Socket
+     */
+    private static void processMessage(DatagramPacket packet, MulticastSocket socket) {
+
+        try {
+            if (new String(packet.getData(), 0, packet.getLength()).equals("RendezVousServer")) { //check if multicast message is meant for this server 
+
+                byte[] input = baseUri.toString().getBytes();
+                DatagramPacket reply = new DatagramPacket(input, input.length);
+
+                //set reply packet destination
+                reply.setAddress(packet.getAddress());
+                reply.setPort(packet.getPort());
+
+                socket.send(reply);
+            }//else ignore message
+        } catch (IOException ex) {
+            System.err.println("Error processing message from client. No reply was sent");
+        }
+    }
 }
